3d058df75c6067de3a8b0c09a011803fde3d226b
benjaurr <baurrutia@uc.cl>
2022-03-27 12:10:56 -0300

    feat(dataset-functions): create function that returns top 10 active users

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from operator import itemgetter
+from collections import defaultdict
 
 def load_information(route):
   tweets = []
@@ -13,3 +14,12 @@ def load_information(route):
 def most_retweeted(tweets):
   sorted_tweets = sorted(tweets, key=itemgetter('retweetCount'), reverse=True)
   return sorted_tweets[0:10]
+
+def most_emited_tweets(tweets):
+  user_tweet_count = defaultdict(lambda: 0)
+
+  for tweet in tweets:
+    user_tweet_count[tweet['user']['username']] += 1
+
+  top_users = sorted(user_tweet_count, key=user_tweet_count.get, reverse=True)[:10]
+  return { k: user_tweet_count[k] for k in top_users }
