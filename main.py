60c48c42dfc079568b2952d728802f4a1e63bf9e
benjaurr <baurrutia@uc.cl>
2022-03-27 12:31:53 -0300

    feat(dataset-functions): create function that returns top 10 tweeted days

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from operator import itemgetter
 from collections import defaultdict
+from datetime import datetime, date
 
 def load_information(route):
   tweets = []
@@ -23,3 +24,12 @@ def most_emited_tweets(tweets):
 
   top_users = sorted(user_tweet_count, key=user_tweet_count.get, reverse=True)[:10]
   return { k: user_tweet_count[k] for k in top_users }
+
+def top_ten_tweeted_days(tweets):
+  date_tweet_count = defaultdict(lambda: 0)
+
+  for tweet in tweets:
+    date_tweet_count[tweet['date'][:10]] += 1
+
+  top_dates = sorted(date_tweet_count, key=date_tweet_count.get, reverse=True)[:10]
+  return { k: date_tweet_count[k] for k in top_dates }
